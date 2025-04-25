2ba62d86f8fa9b0c0bc1072abd381adc86426c40
aniass <a.staskiewicz4@gmail.com>
2025-04-25 18:54:30 +0200

    added file

---
+++ best_model.py
@@ -0,0 +1,74 @@
+"""
+Real Estate price prediction - best model script
+Hyperparameter tuning of Random Forest model
+"""
+
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import r2_score,mean_squared_error
+from sklearn import metrics
+
+
+URL_DATA = r'data\prices_clean.csv'
+
+
+def read_data(path):
+    """Function to read text data"""
+    df = pd.read_csv(URL_DATA)
+    return df
+
+
+def split_and_scale_data(df, target_column='house price', test_size=0.2, random_state=0):
+    '''Function to split data on train and test set'''
+   
+    X = df.drop(columns=target_column)
+    y = df[target_column]
+    X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=test_size, random_state=random_state)
+    
+    scaler = StandardScaler()
+    X_train_scaled = scaler.fit_transform(X_train)
+    X_test_scaled = scaler.transform(X_test)
+    return X_train_scaled, X_test_scaled, y_train, y_test
+
+
+def tune_random_forest_model(X_train, y_train):
+    '''Hyperparameter tuning of Random Forest model'''
+    
+    rf_model = RandomForestRegressor(random_state=42)
+    param_grid = {
+        'max_depth': list(range(1, 10)),
+        'n_estimators': [100, 200, 500, 1000],
+        'min_samples_split': [10, 30, 50]
+    }
+    grid_search = GridSearchCV(rf_model, param_grid, cv=4, n_jobs=-1)
+    grid_search.fit(X_train, y_train)
+    
+    print("Best parameters found:", grid_search.best_params_)
+   
+    best_rf = RandomForestRegressor(**grid_search.best_params_, random_state=42)
+    best_rf.fit(X_train, y_train)
+    return best_rf
+
+
+def evaluate_model(model, X_test, y_test):
+    predictions = model.predict(X_test)
+    print("r2_score:",r2_score(y_test, predictions))
+    #Root mean squared error(RMSE)
+    print("RMSE",np.sqrt(mean_squared_error(y_test, predictions)))
+    #Mean absolute error(MAE)
+    print('MAE',metrics.mean_absolute_error(y_test, predictions))
+    #Mean squared error(MSE)
+    print('MSE',metrics.mean_squared_error(y_test, predictions))
+
+
+
+if __name__ == '__main__':
+    df = read_data(URL_DATA)
+    X_train, X_test, y_train, y_test = split_and_scale_data(df)
+    model = tune_random_forest_model(X_train, X_test, y_train, y_test)
+    # Evaluate the model
+    evaluation = evaluate_model(model, X_test, y_test)
