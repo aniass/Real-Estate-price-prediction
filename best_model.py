cbf921f6a2b447d48d333cfd2c5d5fbfb6bdf588
aniass <a.staskiewicz4@gmail.com>
2025-04-27 18:17:53 +0200

    added main function

--- best_model.py
+++ best_model.py
@@ -1,5 +1,5 @@
 """
-Real Estate price prediction - best model script
+Real Estate price prediction - best model script.
 Hyperparameter tuning of Random Forest model
 """
 
@@ -13,12 +13,12 @@ from sklearn.metrics import r2_score,mean_squared_error
 from sklearn import metrics
 
 
-URL_DATA = r'data\prices_clean.csv'
+DATA_PATH = r'data\prices_clean.csv'
 
 
 def read_data(path):
     """Function to read text data"""
-    df = pd.read_csv(URL_DATA)
+    df = pd.read_csv(DATA_PATH)
     return df
 
 
@@ -27,7 +27,10 @@ def split_and_scale_data(df, target_column='house price', test_size=0.2, random_
    
     X = df.drop(columns=target_column)
     y = df[target_column]
-    X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=test_size, random_state=random_state)
+    
+    X_train, X_test, y_train, y_test = train_test_split(
+        X,y,test_size=test_size, random_state=random_state
+    )
     
     scaler = StandardScaler()
     X_train_scaled = scaler.fit_transform(X_train)
@@ -60,11 +63,15 @@ def evaluate_model(model, X_test, y_test):
     print("Root Mean Squared Error (RMSE):", np.sqrt(mean_squared_error(y_test, predictions)))
     print("Mean Absolute Error (MAE):", metrics.mean_absolute_error(y_test, predictions))
     print("Mean Squared Error (MSE):", metrics.mean_squared_error(y_test, predictions))
- 
+
+
+def main():
+    df = read_data(DATA_PATH)
+    X_train, X_test, y_train, y_test = split_and_scale_data(df)
+    model = tune_random_forest_model(X_train, y_train)
+    evaluate_model(model, X_test, y_test)
+    
 
 if __name__ == '__main__':
-    df = read_data(URL_DATA)
-    X_train, X_test, y_train, y_test = split_and_scale_data(df)
-    model = tune_random_forest_model(X_train, X_test, y_train, y_test)
-    # Evaluate the model
-    evaluation = evaluate_model(model, X_test, y_test)
+    main()
+    
