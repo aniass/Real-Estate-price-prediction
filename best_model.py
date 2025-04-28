8e4461bc3acf37fc2d71e01f6aca86b9de521006
aniass <a.staskiewicz4@gmail.com>
2025-04-28 19:12:28 +0200

    changed docstrings and comments

--- best_model.py
+++ best_model.py
@@ -1,15 +1,14 @@
 """
 Real Estate price prediction - best model script.
 Hyperparameter tuning of Random Forest model
-"""
+ """
 
 import pandas as pd
 import numpy as np
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.preprocessing import StandardScaler
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.model_selection import GridSearchCV
-from sklearn.metrics import r2_score,mean_squared_error
+from sklearn.metrics import r2_score, mean_squared_error
 from sklearn import metrics
 
 
@@ -17,13 +16,13 @@ DATA_PATH = r'data\prices_clean.csv'
 
 
 def read_data(path):
-    """Function to read text data"""
+    '''Reads CSV data from the given path.'''
     df = pd.read_csv(DATA_PATH)
     return df
 
 
 def split_and_scale_data(df, target_column='house price', test_size=0.2, random_state=0):
-    '''Function to split data on train and test set'''
+    '''Function to split data on train and test set.'''
    
     X = df.drop(columns=target_column)
     y = df[target_column]
@@ -31,7 +30,6 @@ def split_and_scale_data(df, target_column='house price', test_size=0.2, random_
     X_train, X_test, y_train, y_test = train_test_split(
         X,y,test_size=test_size, random_state=random_state
     )
-    
     scaler = StandardScaler()
     X_train_scaled = scaler.fit_transform(X_train)
     X_test_scaled = scaler.transform(X_test)
@@ -39,7 +37,8 @@ def split_and_scale_data(df, target_column='house price', test_size=0.2, random_
 
 
 def tune_random_forest_model(X_train, y_train):
-    '''Hyperparameter tuning of Random Forest model'''
+    '''Hyperparameter tuning of Random Forest model.
+       Returns the best estimator.'''
     
     rf_model = RandomForestRegressor(random_state=42)
     param_grid = {
@@ -58,6 +57,7 @@ def tune_random_forest_model(X_train, y_train):
 
 
 def evaluate_model(model, X_test, y_test):
+    '''Evaluates the model and prints various metrics.'''
     predictions = model.predict(X_test)
     print("R^2 Score:", r2_score(y_test, predictions))
     print("Root Mean Squared Error (RMSE):", np.sqrt(mean_squared_error(y_test, predictions)))
